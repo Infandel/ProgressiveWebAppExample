b10f73287df47613922034b142f95c51b4141340
Infandel <tomahawk92@gmail.com>
2021-10-04 22:59:55 +0500

    Updates

---
+++ precache-manifest.bc45e73d1817248f790f839b83dcf218.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92cbb6a86c23af5cf38e5b5bea65375b",
+    "url": "/ProgressiveWebAppExample/index.html"
+  },
+  {
+    "revision": "8a362e1e7a19d8fb3efe",
+    "url": "/ProgressiveWebAppExample/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "79f661df3705dbacc9ab",
+    "url": "/ProgressiveWebAppExample/static/css/main.6574074c.chunk.css"
+  },
+  {
+    "revision": "8a362e1e7a19d8fb3efe",
+    "url": "/ProgressiveWebAppExample/static/js/2.436754dc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ProgressiveWebAppExample/static/js/2.436754dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "79f661df3705dbacc9ab",
+    "url": "/ProgressiveWebAppExample/static/js/main.6629340e.chunk.js"
+  },
+  {
+    "revision": "a8acbab07fd539929172",
+    "url": "/ProgressiveWebAppExample/static/js/runtime-main.f781da83.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/ProgressiveWebAppExample/static/media/SEGA.12a3db8b.woff"
+  }
+]);
